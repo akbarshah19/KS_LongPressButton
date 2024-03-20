9faf0c8bc3ce6b7325e36fcee1cd6067eb653d83
akbarshah19 <105868459+akbarshah19@users.noreply.github.com>
2024-03-21 00:11:40 +0500

    Initial Commit

---
+++ KS_LongPressButtpn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KS_LongPressButtpn
+//
+//  Created by Akbarshah Jumanazarov on 3/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KS_LongPressButtpn/KS_LongPressButtpnApp.swift
@@ -0,0 +1,17 @@
+//
+//  KS_LongPressButtpnApp.swift
+//  KS_LongPressButtpn
+//
+//  Created by Akbarshah Jumanazarov on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct KS_LongPressButtpnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
